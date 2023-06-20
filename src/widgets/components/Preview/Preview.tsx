0abc75af5f5c8dbd751cf37c228f33394232d5c3
EaleksS <ealgenv@gmail.com>
2023-06-20 21:18:02 +0900

    change

--- src/widgets/components/Preview/Preview.tsx
+++ src/widgets/components/Preview/Preview.tsx
@@ -1,37 +1,12 @@
-"use client";
-
-import { FC, useEffect, useRef } from "react";
+import { FC } from "react";
 import styles from "./Preview.module.scss";
 import { Button, Text } from "@/shared";
-import { motion } from "framer-motion";
 import Image from "next/image";
 
 export const Preview: FC = (): JSX.Element => {
-  const img1 = useRef<HTMLImageElement>(null);
-
-  const parallax = (e: MouseEvent) => {
-    if (!img1.current) return;
-
-    const x1 = e.clientX / 1000;
-    const y1 = e.clientY / 500;
-
-    img1.current.style.transform = `translateX(${x1}rem) translateY(${y1}rem)`;
-  };
-
-  useEffect(() => {
-    document.addEventListener("mousemove", parallax);
-
-    return () => document.removeEventListener("mousemove", parallax);
-  }, []);
-
   return (
     <div className={`${styles.preview} container`}>
-      <motion.div
-        className={styles.title}
-        initial={{ y: -200, opacity: 0, scale: 0 }}
-        animate={{ y: 0, opacity: 1, scale: 1 }}
-        transition={{ delay: 0.5 }}
-      >
+      <div className={styles.title}>
         <Text type="h1" color="#fbb531">
           Byster - Авто PvE и PvP
         </Text>
@@ -48,23 +23,10 @@ export const Preview: FC = (): JSX.Element => {
         <Button mt="3rem" type="primary" radius="10px">
           Попробовать бесплатно
         </Button>
-      </motion.div>
-      <motion.div
-        className={styles.img}
-        initial={{ y: -200, opacity: 0, scale: 0 }}
-        animate={{ y: 0, opacity: 1, scale: 1 }}
-        transition={{ delay: 0.5 }}
-      >
-        <Image
-          priority
-          ref={img1}
-          src="/pre.png"
-          alt="img"
-          className={styles.bg}
-          width={600}
-          height={600}
-        />
-      </motion.div>
+      </div>
+      <div>
+        <Image priority src="/pre.png" alt="img" width={600} height={600} />
+      </div>
     </div>
   );
 };
