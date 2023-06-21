0ac034c004bc0a280f85c7f749b62a83f69cde20
EaleksS <ealgenv@gmail.com>
2023-06-21 15:02:54 +0900

    change

--- src/shared/lib/hooks/useWindowDimensions.ts
+++ src/shared/lib/hooks/useWindowDimensions.ts
@@ -3,17 +3,25 @@
 import React from "react";
 
 export const useWindowDimensions = () => {
-  const [width, setWidth] = React.useState<number>(window && window.innerWidth);
-  const [height, setHeight] = React.useState<number>(window && window.innerHeight);
+  const [width, setWidth] = React.useState<number>(
+    typeof window !== undefined ? window.innerWidth : 0
+  );
+  const [height, setHeight] = React.useState<number>(
+    typeof window !== undefined ? window.innerHeight : 0
+  );
 
   const updateWidthAndHeight = () => {
-    setWidth(window &&window.innerWidth);
-    setHeight(window &&window.innerHeight);
+    setWidth(typeof window !== undefined ? window.innerWidth : 0);
+    setHeight(typeof window !== undefined ? window.innerHeight : 0);
   };
 
   React.useEffect(() => {
+    if (typeof window === undefined) return;
+
     window && window.addEventListener("resize", updateWidthAndHeight);
-    return () =>  window && window.removeEventListener("resize", updateWidthAndHeight);
+    
+    return () =>
+      window && window.removeEventListener("resize", updateWidthAndHeight);
   }, []);
 
   return {
