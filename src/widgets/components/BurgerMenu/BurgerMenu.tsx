0d1b0b414b7d29a4db204fd9a3af98f549cc625c
EaleksS <ealgenv@gmail.com>
2023-06-21 15:33:36 +0900

    change

--- src/widgets/components/BurgerMenu/BurgerMenu.tsx
+++ src/widgets/components/BurgerMenu/BurgerMenu.tsx
@@ -19,8 +19,17 @@ export const BurgerMenu: FC<Props> = ({
   useEffect(() => {
     if (isActive) {
       document.body.style.overflowY = "hidden";
+      document.body.addEventListener("touchmove", (e) => e.preventDefault());
+
+      return () =>
+        document.body.removeEventListener("touchmove", (e) =>
+          e.preventDefault()
+        );
     } else {
       document.body.style.overflowY = "auto";
+      document.body.addEventListener("touchmove", (e) => "");
+
+      return () => document.body.removeEventListener("touchmove", (e) => "");
     }
   }, [isActive]);
 
