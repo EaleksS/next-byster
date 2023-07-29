97bc2aebcff314b78846d2f88c5f566067cff3a6
EaleksS <ealgenv@gmail.com>
2023-07-30 02:17:57 +0900

    .

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -1,12 +1,13 @@
+import { defaultLocale } from "@/middleware";
 import { About, Preview, Products, Reviews } from "@/widgets";
 
 export default function Home({ params }: { params: { lang: string } }) {
   return (
     <>
-      <Preview lang={params.lang} />
-      <Products lang={params.lang} />
-      <About lang={params.lang} />
-      <Reviews lang={params.lang} />
+      <Preview lang={params.lang ?? defaultLocale} />
+      <Products lang={params.lang ?? defaultLocale} />
+      <About lang={params.lang ?? defaultLocale} />
+      <Reviews lang={params.lang ?? defaultLocale} />
     </>
   );
 }
