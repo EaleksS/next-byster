ed07fe12a6258de0668ff0689580586b277c07b8
EaleksS <ealgenv@gmail.com>
2023-07-29 19:05:52 +0900

    loader change

--- src/shared/ui/Loader/Loader.tsx
+++ src/shared/ui/Loader/Loader.tsx
@@ -1,28 +1,21 @@
 import { FC } from "react";
-import { ThreeDots } from "react-loader-spinner";
+import { RotatingLines } from "react-loader-spinner";
 import styles from "./Loader.module.scss";
 
 interface Props {
   className?: string;
-  h?: number;
   w?: number;
 }
 
-export const Loader: FC<Props> = ({
-  className,
-  h = 80,
-  w = 80,
-}): JSX.Element => {
+export const Loader: FC<Props> = ({ className, w = 60 }): JSX.Element => {
   return (
     <div className={`${styles.loader} ${className}`}>
-      <ThreeDots
+      <RotatingLines
+        strokeColor="grey"
+        strokeWidth="5"
+        animationDuration="0.75"
+        width={`${w}`}
         visible={true}
-        height={h}
-        width={w}
-        ariaLabel="blocks-loading"
-        wrapperStyle={{}}
-        wrapperClass="blocks-wrapper"
-        color="#fff"
       />
     </div>
   );
