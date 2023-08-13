008ff4765559f0af43d4f0ba1aeaa1db579bbc49
EaleksS <ealgenv@gmail.com>
2023-08-14 02:00:11 +0900

    .

--- next.config.js
+++ next.config.js
@@ -3,11 +3,11 @@ const nextConfig = {
   images: {
     domains: ["s3.byster.one", "api.byster.one"],
   },
-  // i18n: {
-  //   locales: ["ru", "en"],
-  //   defaultLocale: "en",
-  //   localeDetection: true,
-  // },
+  i18n: {
+    locales: ["ru", "en"],
+    defaultLocale: "en",
+    localeDetection: true,
+  },
 };
 
 module.exports = nextConfig;
